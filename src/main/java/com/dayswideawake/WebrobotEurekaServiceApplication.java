e6bd9a5bc87a4c8405f7490f16116c634241885f
Zoltan Nyaray <Zoltan_Nyaray@epam.com>
2016-09-07 18:52:19 +0200

    spring boot init with eureka server

---
+++ src/main/java/com/dayswideawake/WebrobotEurekaServiceApplication.java
@@ -0,0 +1,12 @@
+package com.dayswideawake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebrobotEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebrobotEurekaServiceApplication.class, args);
+	}
+}
