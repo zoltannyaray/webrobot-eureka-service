b524891bdcecb66edf6f3b3735f59ed4fbcd78e6
Zoltan Nyaray <Zoltan_Nyaray@epam.com>
2016-09-08 12:32:33 +0200

    yolo

--- src/test/java/com/dayswideawake/WebrobotEurekaServiceApplicationTests.java
+++ src/test/java/com/dayswideawake/WebrobotEurekaServiceApplicationTests.java
@@ -1,13 +1,11 @@
 package com.dayswideawake;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class WebrobotEurekaServiceApplicationTests {
+public class WebrobotEurekaServiceApplicationTests extends AbstractTestNGSpringContextTests {
 
 	@Test
 	public void contextLoads() {
